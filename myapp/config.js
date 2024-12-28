0cfe2358385422a43b471f096cfd07541dfba91f
MarioSaputa32 <remgremory32@gmail.com>
2024-12-29 03:23:23 +0700

    hellooooo

--- myapp/config.js
+++ myapp/config.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('pesan_journeyran', 'pesan_journeyran', 'ba436de7d82ae622ddd94a3461e03dc42cebbed5', {
     host: 's3lws.h.filess.io',
     dialect: 'mysql',
+    dialectModule: require('mysql2'),
     pool: {
         max: 5, // Maksimal koneksi aktif
         min: 0, // Minimal koneksi aktif
