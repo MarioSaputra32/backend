9e903de8c194107650fe9d4e7795eb6eb1776ec6
MarioSaputa32 <remgremory32@gmail.com>
2024-12-29 02:53:40 +0700

    ba

--- myapp/config.js
+++ myapp/config.js
@@ -1,9 +1,14 @@
-const { Module } = require('module');
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('pesan_journeyran','pesan_journeyran','ba436de7d82ae622ddd94a3461e03dc42cebbed5',{
+const sequelize = new Sequelize('pesan_journeyran', 'pesan_journeyran', 'ba436de7d82ae622ddd94a3461e03dc42cebbed5', {
     host: 's3lws.h.filess.io',
     dialect: 'mysql',
+    pool: {
+        max: 5, // Maksimal koneksi aktif
+        min: 0, // Minimal koneksi aktif
+        acquire: 30000, // Waktu maksimal (dalam ms) untuk memperoleh koneksi sebelum timeout
+        idle: 10000, // Waktu koneksi idle sebelum dilepaskan (dalam ms)
+    },
 });
 
 module.exports = sequelize;
